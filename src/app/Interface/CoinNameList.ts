7c1bd6e1d9e1a206052554cca97bbcd1c8081db8
ANANTH4004 <balamuruganananth@gmail.com>
2023-07-08 22:14:59 +0530

    Extra some interface added

---
+++ src/app/Interface/CoinNameList.ts
@@ -0,0 +1,29 @@
+
+export interface CoinNameList {
+    data: CoinInfo[]
+    info: Info
+  }
+
+  export interface CoinInfo {
+    id: string
+    symbol: string
+    name: string
+    nameid: string
+    rank: number
+    price_usd: string
+    percent_change_24h: string
+    percent_change_1h: string
+    percent_change_7d: string
+    price_btc: string
+    market_cap_usd: string
+    volume24: number
+    volume24a: number
+    csupply: string
+    tsupply?: string
+    msupply?: string
+  }
+
+  export interface Info {
+    coins_num: number
+    time: number
+  }
